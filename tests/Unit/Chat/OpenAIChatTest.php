2244eab2d40abdca44e0478626f77be85697df00
aaronaccessvr <acollegeman@accessvr.com>
2025-05-18 11:22:26 -0400

    Try to account for requests these mocks should receive during testing

--- tests/Unit/Chat/OpenAIChatTest.php
+++ tests/Unit/Chat/OpenAIChatTest.php
@@ -211,13 +211,13 @@ it('does not throw away "0" strings when creating streamed response', function (
     $response->allows([
         'getBody' => $stream,
     ]);
-    $response->shouldReceive('getStatusCode')->andReturn(200);
+    $response->shouldReceive('getHeaders');
 
     $transport = Mockery::mock(TransporterContract::class);
     $transport->allows([
         'requestStream' => $response,
     ]);
-    $transport->shouldReceive('requestObject')->andReturn($response);
+    $transport->shouldReceive('requestObject');
 
     $config = new OpenAIConfig();
     $config->client = new Client($transport);
