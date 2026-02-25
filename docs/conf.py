aadb6d90afd1a748edf7f83f6e50b400438f32b6
Tom H Anderson <tom.h.anderson@gmail.com>
2026-02-24 19:02:05 -0700

    Fix docs title

--- docs/conf.py
+++ docs/conf.py
@@ -13,8 +13,8 @@ master_doc = 'index'
 project = u'LLPhant'
 copyright = u'MIT License'
 version = '9'
-html_title = "A comprehensive PHP Generative AI Framework for building AI agents, chatbots, and more."
-html_short_title = "A comprehensive PHP Generative AI Framework"
+html_title = "A comprehensive PHP generative AI framework for building AI agents, chatbots, and more."
+html_short_title = "A Comprehensive PHP Generative AI Framework"
 html_favicon = 'assets/favicon.ico'
 
 exclude_patterns = ['_build']
