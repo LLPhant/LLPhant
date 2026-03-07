c09ac4d3dd81169313ed2ba487c26a71de289cf4
Franco Lombardo <f.lombardo69@gmail.com>
2026-03-07 11:11:51 +0100

    Add support for PHP 8.5

--- rector.php
+++ rector.php
@@ -2,23 +2,16 @@
 
 declare(strict_types=1);
 
-use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
-use Rector\CodeQuality\Rector\Empty_\SimplifyEmptyCheckOnEmptyArrayRector;
-use Rector\CodeQuality\Rector\If_\ExplicitBoolCompareRector;
-use Rector\Config\RectorConfig;
-use Rector\EarlyReturn\Rector\If_\ChangeAndIfToEarlyReturnRector;
-use Rector\EarlyReturn\Rector\If_\ChangeOrIfReturnToEarlyReturnRector;
-use Rector\Php56\Rector\FunctionLike\AddDefaultValueForUndefinedVariableRector;
-use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
-use Rector\Privatization\Rector\Class_\ChangeGlobalVariablesToPropertiesRector;
-use Rector\Privatization\Rector\Class_\ChangeReadOnlyVariableWithDefaultValueToConstantRector;
+use Rector\CodeQuality\Rector\BooleanAnd\RepeatedAndNotEqualToNotInArrayRector;use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+use Rector\CodeQuality\Rector\ClassMethod\LocallyCalledStaticMethodToNonStaticRector;use Rector\CodeQuality\Rector\Empty_\SimplifyEmptyCheckOnEmptyArrayRector;
+use Rector\CodeQuality\Rector\Equal\UseIdenticalOverEqualWithSameTypeRector;use Rector\CodeQuality\Rector\If_\ExplicitBoolCompareRector;
+use Rector\CodingStyle\Rector\FuncCall\FunctionFirstClassCallableRector;use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\Cast\RecastingRemovalRector;use Rector\DeadCode\Rector\If_\RemoveAlwaysTrueIfConditionRector;use Rector\Php70\Rector\StmtsAwareInterface\IfIssetToCoalescingRector;use Rector\Php74\Rector\Property\RestoreDefaultNullToNullableTypePropertyRector;use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
 use Rector\Privatization\Rector\ClassMethod\PrivatizeFinalClassMethodRector;
 use Rector\Privatization\Rector\MethodCall\PrivatizeLocalGetterToPropertyRector;
-use Rector\Privatization\Rector\Property\ChangeReadOnlyPropertyWithDefaultValueToConstantRector;
 use Rector\Privatization\Rector\Property\PrivatizeFinalClassPropertyRector;
 use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
-use Rector\TypeDeclaration\Rector\ClassMethod\ArrayShapeFromConstantArrayReturnRector;
+use Rector\Set\ValueObject\SetList;use Rector\Strict\Rector\Empty_\DisallowedEmptyRuleFixerRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
@@ -28,9 +21,6 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rules([
         InlineConstructorDefaultToPropertyRector::class,
         ClassPropertyAssignToConstructorPromotionRector::class,
-        ChangeGlobalVariablesToPropertiesRector::class,
-        ChangeReadOnlyPropertyWithDefaultValueToConstantRector::class,
-        ChangeReadOnlyVariableWithDefaultValueToConstantRector::class,
         PrivatizeLocalGetterToPropertyRector::class,
         PrivatizeFinalClassPropertyRector::class,
         PrivatizeFinalClassMethodRector::class,
@@ -45,10 +35,31 @@ return static function (RectorConfig $rectorConfig): void {
 
     $rectorConfig->skip([
         SimplifyEmptyCheckOnEmptyArrayRector::class,
-        ArrayShapeFromConstantArrayReturnRector::class,
-        AddDefaultValueForUndefinedVariableRector::class,
-        ChangeAndIfToEarlyReturnRector::class,
-        ChangeOrIfReturnToEarlyReturnRector::class,
-        ExplicitBoolCompareRector::class
+        ExplicitBoolCompareRector::class,
+        RecastingRemovalRector::class,
+        DisallowedEmptyRuleFixerRector::class,
+        FunctionFirstClassCallableRector::class,
+        LocallyCalledStaticMethodToNonStaticRector::class,
+        IfIssetToCoalescingRector::class,
+        FunctionFirstClassCallableRector::class,
+        RepeatedAndNotEqualToNotInArrayRector::class,
+        DisallowedEmptyRuleFixerRector::class,
+        RecastingRemovalRector::class,
+        DisallowedEmptyRuleFixerRector::class,
+        RepeatedAndNotEqualToNotInArrayRector::class,
+        DisallowedEmptyRuleFixerRector::class,
+        RemoveAlwaysTrueIfConditionRector::class,
+        UseIdenticalOverEqualWithSameTypeRector::class,
+        FunctionFirstClassCallableRector::class,
+        DisallowedEmptyRuleFixerRector::class,
+        FunctionFirstClassCallableRector::class,
+        FunctionFirstClassCallableRector::class,
+        RestoreDefaultNullToNullableTypePropertyRector::class,
+        DisallowedEmptyRuleFixerRector::class,
+        DisallowedEmptyRuleFixerRector::class,
+        RecastingRemovalRector::class,
+        DisallowedEmptyRuleFixerRector::class,
+        DisallowedEmptyRuleFixerRector::class,
+        RecastingRemovalRector::class,
     ]);
 };
